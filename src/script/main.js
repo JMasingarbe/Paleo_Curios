e84241bcea07d2ee7fb2797b93d247e9b78a18dc
JMasingarbe <juliette.masingarbe2002@gmail.com>
2026-08-12 12:52:31 +0200

    Fully responsive header & footer AND phone & tablet home.html style

--- src/script/main.js
+++ src/script/main.js
@@ -1,11 +1,9 @@
-// Test menu responsive header
+// Responsive menu header
 const phone_nav_button = document.querySelector('#phone_nav_button');
 const ul_main_nav = document.querySelector('nav ul');
 const menu_icons = document.querySelectorAll('#phone_nav_button div');
-console.log(menu_icons)
-if (phone_nav_button.style.display !== 'none') {
-    ul_main_nav.style.display = 'none';
-}
+
+window.innerWidth >= 768 ? ul_main_nav.style.display = '' : ul_main_nav.style.display = 'none';
 
 phone_nav_button.addEventListener('click', ()=> {
     ul_main_nav.style.display == 'none' ? ul_main_nav.style.display = 'block' : ul_main_nav.style.display = 'none';
