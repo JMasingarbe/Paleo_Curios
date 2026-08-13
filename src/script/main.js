e31cbe60572357bde7261d9f34334bd5d130386f
JMasingarbe <juliette.masingarbe2002@gmail.com>
2026-08-13 12:18:10 +0200

    MINOR: html update footer from home.html

--- src/script/main.js
+++ src/script/main.js
@@ -2,14 +2,11 @@
 const phone_nav_button = document.querySelector('#phone_nav_button');
 const ul_main_nav = document.querySelector('nav ul');
 const menu_icons = document.querySelectorAll('#phone_nav_button div');
-
-window.innerWidth >= 768 ? ul_main_nav.style.display = '' : ul_main_nav.style.display = 'none';
-
+    // Listen click to open / close menu on phone
 phone_nav_button.addEventListener('click', ()=> {
-    ul_main_nav.style.display == 'none' ? ul_main_nav.style.display = 'block' : ul_main_nav.style.display = 'none';
-    // if (ul_main_nav.style.display !== 'none') {
-        for (menu_icon of menu_icons) {
-            menu_icon.classList.toggle('change');
-        }
-    // }
+    ul_main_nav.style.display === 'none' || ul_main_nav.style.display === '' ? ul_main_nav.style.display = 'block' : ul_main_nav.style.display = 'none';
+    // Toggle menu icon animation
+    for (menu_icon of menu_icons) {
+        menu_icon.classList.toggle('change');
+    }
 })
